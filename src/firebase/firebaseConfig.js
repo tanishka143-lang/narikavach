9087d1c892847f876cfd01287af0bb68c05ea80a
Tanny Candy <tannycandy143@gmail.com>
2026-05-16 20:00:02 +0530

    Move Firebase config to environment variables

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,24 +1,17 @@
-// Import the functions you need from the SDKs you need
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBoujP-QvncZK1JqtfiwQMXhO0pnEsKZ-M",
-  authDomain: "narikavach.firebaseapp.com",
-  projectId: "narikavach",
-  storageBucket: "narikavach.firebasestorage.app",
-  messagingSenderId: "706703712110",
-  appId: "1:706703712110:web:62e28c1bb159d7cea9ab7f",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-
-export default app;
